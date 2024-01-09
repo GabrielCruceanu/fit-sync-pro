7ed3c3818d3ab85182a89ce068e8e980517b5171
Kaapo.Studio <contact@kaapo.studio>
2024-01-09 17:28:27 +0200

    react-datepicker/dist/react-datepicker.css deleted

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import { Providers } from "@/app/providers/NextUiProvider";
 import { Inter } from "next/font/google";
 import { defaultUrl } from "@/helpers/helpers";
-import "react-datepicker/dist/react-datepicker.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
